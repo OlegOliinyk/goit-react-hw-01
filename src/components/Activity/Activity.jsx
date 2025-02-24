b3ddf338e08d2092944ee2b8c86fdb66293cc37b
OlegOliinyk <91748610+OlegOliinyk@users.noreply.github.com>
2025-02-24 21:58:13 +0200

    Correct 01

--- src/components/Activity/Activity.jsx
+++ src/components/Activity/Activity.jsx
@@ -2,7 +2,7 @@ import css from './Activity.module.css';
 
 function Activity({ label, quantity }) {
   return (
-    <div className={css.Activity}>
+    <div className={css.activity}>
       <span className={css.label}>{label}</span>
       <span className={css.quantity}>{quantity}</span>
     </div>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,27 +1,48 @@
 import Activity from '../Activity/Activity';
-import css from '../Profile/Profile.module.css';
 import UserDetails from '../UserDetails/UserDetails';
+import css from '../Profile/Profile.module.css';
 
 function Profile({ name, tag, location, image, stats }) {
-  <div className={css.profileCard}>
-    <UserDetails
-      name={name}
-      tag={tag}
-      location={location}
-      image={image}
-    ></UserDetails>
-    <ul>
-      <li>
-        <Activity label="Followers" quantity={stats.followers}></Activity>
-      </li>
-      <li>
-        <Activity label="Views" quantity={stats.views}></Activity>
-      </li>
-      <li>
-        <Activity label="Likes" quantity={stats.likes}></Activity>
-      </li>
-    </ul>
-  </div>;
+  console.log(css);
+  return (
+    <div className={css.profileCard}>
+      <UserDetails
+        name={name}
+        tag={tag}
+        location={location}
+        image={image}
+      ></UserDetails>
+      {/* <div className={css.details}>
+        <img src={image} alt="User avatar" />
+        <p>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div> */}
+      <ul>
+        <li>
+          <Activity label="Followers" quantity={stats.followers}></Activity>
+          {/* <div className={css.activity}>
+            <span className={css.label}>Followers</span>
+            <span className={css.quantity}>{stats.followers}</span>
+          </div> */}
+        </li>
+        <li>
+          <Activity label="Views" quantity={stats.views}></Activity>
+          {/* <div className={css.activity}>
+            <span className={css.label}>Views</span>
+            <span className={css.quantity}>{stats.views}</span>
+          </div> */}
+        </li>
+        <li>
+          <Activity label="Likes" quantity={stats.likes}></Activity>
+          {/* <div className={css.activity}>
+            <span className={css.label}>Likes</span>
+            <span className={css.quantity}>{stats.likes}</span>
+          </div> */}
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Profile;
